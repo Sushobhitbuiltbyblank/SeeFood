2db1572e96efc6b263ef4835b6f3a7d790e87e6e
sushobhit jain <sushobhitt.builtbyblank@gmail.com>
2025-05-24 22:45:21 +0530

    Initial Commit

---
+++ SeeFood/SeeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeFoodApp.swift
+//  SeeFood
+//
+//  Created by Sushobhit Jain on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SeeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
